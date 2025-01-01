3a5c97c777dc77f1016cca0a5579d569aa253bd6
Sanjjith Dineshkumar <sanjjith.dinesh79@gmail.com>
2024-12-31 20:08:52 -0500

    fixed express installation issues and created first few basic routes for api

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,25 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
 
 const app = express();
+dotenv.config();
 
-//temporary port 
-app.listen(5001, () => console.log("Server started on port 5001"));
+app.get('/', (req,res) => {
+    res.send("API is Running");
+});
+
+app.get('/api/chat', (req,res) =>{
+    res.send(chats);
+});
+
+app.get('/api/chat/:id', (req,res) => {        
+     //console.log(req.params.id);
+     const singleChat = chats.find((c) => c._id === req.params.id);
+     res.send(singleChat);
+});
+
+const PORT = process.env.PORT || 5001;
+
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
