876ac12299e818607023b53209c52f38cf317af4
Sanjjith Dineshkumar <sanjjith.dinesh79@gmail.com>
2025-01-06 16:17:12 -0500

    added message routes amd controller functionality and basic cloudinary integration

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,9 +3,9 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import { connectDB } from "./lib/db.js";
-import authRoutes from "./routes/auth.route.js";
-import { connect } from "http2";
 
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 dotenv.config();
 
 const app = express();
@@ -16,6 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("server is running on PORT: " + PORT);
