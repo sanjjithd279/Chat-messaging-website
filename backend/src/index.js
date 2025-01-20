bdcc1ea1c293decf5e2f3ece966e0135bb475621
Sanjjith Dineshkumar <sanjjith.dinesh79@gmail.com>
2025-01-20 17:17:00 -0500

    created socket connection and real time notification if user is online

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,9 +7,11 @@ import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js"; 
+
 dotenv.config();
 
-const app = express();
+
 
 const PORT = process.env.PORT;
 
@@ -25,7 +27,7 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log("server is running on PORT: " + PORT);
   connectDB();
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ import { Loader } from "lucide-react";
 import { Toaster } from "react-hot-toast";
 
 const App = () => {
-  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+  const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
   const { theme } = useThemeStore();
 
   useEffect(() => {
